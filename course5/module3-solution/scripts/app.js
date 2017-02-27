39ec9724107ee6601feadcee1a35f4cf4485a21e
klamath <palomino2004@gmail.com>
2017-02-28 01:54:16 +0200

    module3 solution implemented

---
+++ course5/module3-solution/scripts/app.js
@@ -0,0 +1,66 @@
+(function(){
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', foundItems);
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService){
+    var ctrl = this;
+    ctrl.found = [];
+    ctrl.searchTerm = '';
+    ctrl.search = function(){
+      if (this.searchTerm != '') {
+        MenuSearchService.getMatchedMenuItems(this.searchTerm).then(function (result) {
+          console.log('RESULT: ', result);
+          ctrl.found = result
+        });
+      }
+      else {
+        console.log('Please prvide search criteria');
+      }
+      console.log('search result: ', ctrl.found);
+    };
+
+    ctrl.onRemove = function(index){
+      ctrl.found.splice(index, 1);
+    };
+  }
+
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http){
+    var itemsUrl = 'https://davids-restaurant.herokuapp.com/menu_items.json';
+    this.getMatchedMenuItems = function(searchTerm){
+      return $http({method: 'GET', url: itemsUrl}).then(
+        function (result) {
+          // process result and only keep items that match
+          var foundItems = [];
+          for (var i = 0; i < result.data.menu_items.length; i++) {
+            if (result.data.menu_items[i].description.indexOf(searchTerm.toLowerCase().trim()) !== -1){
+              foundItems.push(result.data.menu_items[i]);
+            }
+          }
+
+          // return processed items
+          return foundItems;
+        },
+        function(){
+          throw new Error('Error while fetching data from server');
+        });
+    };
+  };
+
+  function foundItems() {
+    var ddo = {
+      scope: {
+        items: '<foundItems',
+        removeItem: '&onRemove'
+      },
+      templateUrl: './scripts/templates/found_items.html',
+      restrict: 'E'
+    }
+    return ddo;
+  };
+})()
